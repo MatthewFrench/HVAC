aa40d0ba7935757122101f339ce197923c1c8e1e
austin03 <austin03@ksu.edu>
2016-09-16 11:13:54 -0500

    Inserted View and Delete Button

--- HVAC Model Designer/Javascript/HVACApplication.js
+++ HVAC Model Designer/Javascript/HVACApplication.js
@@ -2,7 +2,8 @@
  * Created by Matt on 9/9/16.
  */
 
-var LAYOUT_MODE_CREATE_WALL = 0, LAYOUT_MODE_EDIT_WALL = 1, LAYOUT_MODE_DRAG = 2;
+var LAYOUT_MODE_CREATE_WALL = 0, LAYOUT_MODE_EDIT_WALL = 1, LAYOUT_MODE_DRAG = 2, LAYOUT_MODE_VIEW = 3,
+    LAYOUT_MODE_DELETE = 4;
 var WALL_POINT_ONE = 1, WALL_POINT_CENTER = 2, WALL_POINT_TWO = 2;
 
 //Constructor
@@ -53,6 +54,14 @@ HVACApplication.prototype.createUI = function() {
     };
 
 
+    this.viewButtonDiv = document.createElement("div");
+    this.viewButtonDiv.className = "ViewButtonDiv";
+    this.viewButtonDiv.innerText = "View";
+    this.viewButtonDiv.onclick = function(event) {
+        "use strict";
+        self.viewWallButtonClicked();
+    }
+    document.body.appendChild(this.viewButtonDiv);
 
     this.dragButtonDiv = document.createElement("div");
     this.dragButtonDiv.className = "DragButtonDiv";
@@ -80,6 +89,15 @@ HVACApplication.prototype.createUI = function() {
         self.editWallButtonClicked();
     }
     document.body.appendChild(this.editButtonDiv);
+
+    this.deleteButtonDiv = document.createElement("div");
+    this.deleteButtonDiv.className = "DeleteButtonDiv";
+    this.deleteButtonDiv.innerText = "Delete";
+    this.deleteButtonDiv.onclick = function(event) {
+        "use strict";
+        self.deleteWallButtonClicked();
+    }
+    document.body.appendChild(this.deleteButtonDiv);
 
     this.resizeCanvas();
 
@@ -143,7 +161,6 @@ HVACApplication.prototype.resizeCanvas = function() {
     this.layoutCanvas.width = window.innerWidth;
     this.layoutCanvas.height = window.innerHeight - 150;
 }
-
 
 HVACApplication.prototype.layoutCanvasMousePressed = function(event) {
     "use strict";
@@ -319,27 +336,52 @@ HVACApplication.prototype.layoutCanvasMouseMoved = function(event) {
     }
 }
 
+HVACApplication.prototype.viewWallButtonClicked = function() {
+    "use strict";
+    this.currentLayoutMode = LAYOUT_MODE_VIEW;
+    this.viewButtonDiv.className = "ViewButtonDiv selectedButtonDiv";
+    this.dragButtonDiv.className = "DragButtonDiv";
+    this.createButtonDiv.className = "CreateButtonDiv";
+    this.editButtonDiv.className = "EditButtonDiv";
+    this.deleteButtonDiv.className = "DeleteButtonDiv";
+};
 HVACApplication.prototype.dragButtonClicked = function() {
     "use strict";
     this.currentLayoutMode = LAYOUT_MODE_DRAG;
+    this.viewButtonDiv.className = "ViewButtonDiv";
     this.dragButtonDiv.className = "DragButtonDiv selectedButtonDiv";
     this.createButtonDiv.className = "CreateButtonDiv";
     this.editButtonDiv.className = "EditButtonDiv";
+    this.deleteButtonDiv.className = "DeleteButtonDiv";
 };
 HVACApplication.prototype.createWallButtonClicked = function() {
     "use strict";
     this.currentLayoutMode = LAYOUT_MODE_CREATE_WALL;
+    this.viewButtonDiv.className = "ViewButtonDiv";
     this.dragButtonDiv.className = "DragButtonDiv";
     this.createButtonDiv.className = "CreateButtonDiv selectedButtonDiv";
     this.editButtonDiv.className = "EditButtonDiv";
+    this.deleteButtonDiv.className = "DeleteButtonDiv";
 };
 HVACApplication.prototype.editWallButtonClicked = function() {
     "use strict";
     this.currentLayoutMode = LAYOUT_MODE_EDIT_WALL;
+    this.viewButtonDiv.className = "ViewButtonDiv";
     this.dragButtonDiv.className = "DragButtonDiv";
     this.createButtonDiv.className = "CreateButtonDiv";
     this.editButtonDiv.className = "EditButtonDiv selectedButtonDiv";
+    this.deleteButtonDiv.className = "DeleteButtonDiv";
 };
+HVACApplication.prototype.deleteWallButtonClicked = function() {
+    "use strict";
+    this.currentLayoutMode = LAYOUT_MODE_DELETE;
+    this.viewButtonDiv.className = "ViewButtonDiv";
+    this.dragButtonDiv.className = "DragButtonDiv";
+    this.createButtonDiv.className = "CreateButtonDiv";
+    this.editButtonDiv.className = "EditButtonDiv";
+    this.deleteButtonDiv.className = "DeleteButtonDiv selectedButtonDiv";
+};
+
 HVACApplication.prototype.onKeydown = function(event) {
     "use strict";
     //var key = event.which;
