aeddd2d9e1d6353e76074fcd618a6d710293248b
MatthewFrench <matthewf@ksu.edu>
2016-09-23 10:54:44 -0500

    Revert "Test"
    This reverts commit d08cd95331f12af01c25d89c3c65c93d2be8092f.

--- HVAC Model Designer/Javascript/HVACApplication.js
+++ HVAC Model Designer/Javascript/HVACApplication.js
@@ -2,12 +2,7 @@
  * Created by Matt on 9/9/16.
  */
 
-<<<<<<< HEAD
 var LAYOUT_MODE_CREATE_WALL = 0, LAYOUT_MODE_EDIT = 1, LAYOUT_MODE_DRAG = 2;
-=======
-var LAYOUT_MODE_CREATE_WALL = 0, LAYOUT_MODE_EDIT_WALL = 1, LAYOUT_MODE_DRAG = 2, LAYOUT_MODE_VIEW = 3,
-    LAYOUT_MODE_DELETE = 4;
->>>>>>> parent of 23f592b... Added Functionality to Delete Button.
 var WALL_POINT_ONE = 1, WALL_POINT_CENTER = 2, WALL_POINT_TWO = 2;
 var EDIT_MODE_POINT = 0, EDIT_MODE_CORNER = 1, EDIT_MODE_WALL = 2;
 
@@ -22,84 +17,8 @@ var HVACApplication = function () {
     this.previousMouseX = 0.0;
     this.previousMouseY = 0.0;
     this.mouseDown = false;
-<<<<<<< HEAD
     this.currentLayoutMode = LAYOUT_MODE_CREATE_WALL;
     this.currentEditMode = EDIT_MODE_POINT;
-=======
-
-    this.createUI();
-};
-
-HVACApplication.prototype.createUI = function() {
-    this.myBannerDiv = document.createElement("div");
-    this.myBannerDiv.className = "RibbonBanner";
-    document.body.appendChild(this.myBannerDiv);
-
-    this.titleSpan = document.createElement("span");
-    this.titleSpan.className = "TopTitle";
-    this.titleSpan.innerText = "HVAC Model Designer";
-    document.body.appendChild(this.titleSpan);
-
-    this.layoutCanvas = document.createElement("canvas");
-    this.layoutCanvas.className = "LayoutCanvas";
-    document.body.appendChild(this.layoutCanvas);
-    var self = this;
-    document.body.onmousemove = function(event){
-        self.layoutCanvasMouseMoved(event);
-    };
-    this.layoutCanvas.onmousedown = function(event){
-        self.layoutCanvasMousePressed(event);
-    };
-    this.layoutCanvas.onmouseup = function(event){
-        self.layoutCanvasMouseReleased(event);
-    };
-
-
-    this.viewButtonDiv = document.createElement("div");
-    this.viewButtonDiv.className = "ViewButtonDiv";
-    this.viewButtonDiv.innerText = "View";
-    this.viewButtonDiv.onclick = function(event) {
-        "use strict";
-        self.viewWallButtonClicked();
-    }
-    document.body.appendChild(this.viewButtonDiv);
-
-    this.dragButtonDiv = document.createElement("div");
-    this.dragButtonDiv.className = "DragButtonDiv";
-    this.dragButtonDiv.innerText = "Drag";
-    this.dragButtonDiv.onclick = function(event) {
-        "use strict";
-        self.dragButtonClicked();
-    };
-    document.body.appendChild(this.dragButtonDiv);
-
-    this.createButtonDiv = document.createElement("div");
-    this.createButtonDiv.className = "CreateButtonDiv";
-    this.createButtonDiv.innerText = "Create";
-    this.createButtonDiv.onclick = function(event) {
-        "use strict";
-        self.createWallButtonClicked();
-    };
-    document.body.appendChild(this.createButtonDiv);
-
-    this.editButtonDiv = document.createElement("div");
-    this.editButtonDiv.className = "EditButtonDiv";
-    this.editButtonDiv.innerText = "Edit";
-    this.editButtonDiv.onclick = function(event) {
-        "use strict";
-        self.editWallButtonClicked();
-    }
-    document.body.appendChild(this.editButtonDiv);
-
-    this.deleteButtonDiv = document.createElement("div");
-    this.deleteButtonDiv.className = "DeleteButtonDiv";
-    this.deleteButtonDiv.innerText = "Delete";
-    this.deleteButtonDiv.onclick = function(event) {
-        "use strict";
-        self.deleteWallButtonClicked();
-    }
-    document.body.appendChild(this.deleteButtonDiv);
->>>>>>> parent of 23f592b... Added Functionality to Delete Button.
 
     this.initCreateModeVariables();
     this.initDragModeVariables();
@@ -136,25 +55,7 @@ HVACApplication.prototype.layoutDraw = function() {
             this.drawEditWallModeLayout();
         }
     }
-<<<<<<< HEAD
 };
-=======
-
-    for (var i = 0; i < this.wallList.length; i++) {
-        var wall = this.wallList[i];
-        wall.draw(ctx, this.currentLayoutMode == LAYOUT_MODE_EDIT_WALL);
-    }
-
-    if (this.currentCreateWall != null) {
-        this.currentCreateWall.drawLength(ctx);
-    }
-    if (this.selectedWall != null) {
-        this.selectedWall.drawLength(ctx);
-    }
-
-    ctx.restore();
-}
->>>>>>> parent of 23f592b... Added Functionality to Delete Button.
 HVACApplication.prototype.windowResized = function() {
     this.resizeCanvas();
 };
@@ -186,7 +87,6 @@ HVACApplication.prototype.layoutCanvasMousePressed = function(event) {
         if (this.currentEditMode == EDIT_MODE_POINT) {
             this.mousePressedEditPointModeLayout();
         }
-<<<<<<< HEAD
         if (this.currentEditMode == EDIT_MODE_CORNER) {
             this.mousePressedEditCornerModeLayout();
         }
@@ -195,33 +95,15 @@ HVACApplication.prototype.layoutCanvasMousePressed = function(event) {
         }
     }
 };
-=======
-    }
-    if (this.currentLayoutMode == LAYOUT_MODE_EDIT_WALL) {
-        this.selectedWall = null;
-    }
-}
->>>>>>> parent of 23f592b... Added Functionality to Delete Button.
 
 HVACApplication.prototype.layoutCanvasMouseMoved = function(event) {
     "use strict";
     var mouseX = event.clientX - this.layoutCanvas.offsetLeft;
     var mouseY = event.clientY - this.layoutCanvas.offsetTop;
-<<<<<<< HEAD
     this.previousMouseX = this.currentMouseX;
     this.currentMouseX = mouseX;
     this.previousMouseY = this.currentMouseY;
     this.currentMouseY = mouseY;
-=======
-    var movedX = this.lastMouseX - mouseX;
-    var movedY = this.lastMouseY - mouseY;
-    this.lastMouseX = mouseX;
-    this.lastMouseY = mouseY;
-
-
-    var canvasMouseX = mouseX - this.dragPositionX;
-    var canvasMouseY = mouseY - this.dragPositionY;
->>>>>>> parent of 23f592b... Added Functionality to Delete Button.
 
     if (this.currentLayoutMode == LAYOUT_MODE_CREATE_WALL) {
         this.mouseMovedCreateModeLayout();
@@ -259,7 +141,6 @@ HVACApplication.prototype.layoutCanvasMouseReleased = function(event) {
     if (this.currentLayoutMode == LAYOUT_MODE_DRAG) {
         this.mouseReleasedDragModeLayout();
     }
-<<<<<<< HEAD
     if (this.currentLayoutMode == LAYOUT_MODE_EDIT) {
         if (this.currentEditMode == EDIT_MODE_POINT) {
             this.mouseReleasedEditPointModeLayout();
@@ -271,54 +152,6 @@ HVACApplication.prototype.layoutCanvasMouseReleased = function(event) {
             this.mouseReleasedEditWallModeLayout();
         }
     }
-=======
-}
-
-HVACApplication.prototype.viewWallButtonClicked = function() {
-    "use strict";
-    this.currentLayoutMode = LAYOUT_MODE_VIEW;
-    this.viewButtonDiv.className = "ViewButtonDiv selectedButtonDiv";
-    this.dragButtonDiv.className = "DragButtonDiv";
-    this.createButtonDiv.className = "CreateButtonDiv";
-    this.editButtonDiv.className = "EditButtonDiv";
-    this.deleteButtonDiv.className = "DeleteButtonDiv";
-};
-HVACApplication.prototype.dragButtonClicked = function() {
-    "use strict";
-    this.currentLayoutMode = LAYOUT_MODE_DRAG;
-    this.viewButtonDiv.className = "ViewButtonDiv";
-    this.dragButtonDiv.className = "DragButtonDiv selectedButtonDiv";
-    this.createButtonDiv.className = "CreateButtonDiv";
-    this.editButtonDiv.className = "EditButtonDiv";
-    this.deleteButtonDiv.className = "DeleteButtonDiv";
-};
-HVACApplication.prototype.createWallButtonClicked = function() {
-    "use strict";
-    this.currentLayoutMode = LAYOUT_MODE_CREATE_WALL;
-    this.viewButtonDiv.className = "ViewButtonDiv";
-    this.dragButtonDiv.className = "DragButtonDiv";
-    this.createButtonDiv.className = "CreateButtonDiv selectedButtonDiv";
-    this.editButtonDiv.className = "EditButtonDiv";
-    this.deleteButtonDiv.className = "DeleteButtonDiv";
-};
-HVACApplication.prototype.editWallButtonClicked = function() {
-    "use strict";
-    this.currentLayoutMode = LAYOUT_MODE_EDIT_WALL;
-    this.viewButtonDiv.className = "ViewButtonDiv";
-    this.dragButtonDiv.className = "DragButtonDiv";
-    this.createButtonDiv.className = "CreateButtonDiv";
-    this.editButtonDiv.className = "EditButtonDiv selectedButtonDiv";
-    this.deleteButtonDiv.className = "DeleteButtonDiv";
-};
-HVACApplication.prototype.deleteWallButtonClicked = function() {
-    "use strict";
-    this.currentLayoutMode = LAYOUT_MODE_DELETE;
-    this.viewButtonDiv.className = "ViewButtonDiv";
-    this.dragButtonDiv.className = "DragButtonDiv";
-    this.createButtonDiv.className = "CreateButtonDiv";
-    this.editButtonDiv.className = "EditButtonDiv";
-    this.deleteButtonDiv.className = "DeleteButtonDiv selectedButtonDiv";
->>>>>>> parent of 23f592b... Added Functionality to Delete Button.
 };
 
 
