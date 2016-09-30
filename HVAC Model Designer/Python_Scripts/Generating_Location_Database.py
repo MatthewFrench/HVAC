b730bd6b8240af0d06f8b260ceb2c92ad48a088a
Andrew Massey <amassey7@ksu.edu>
2016-09-30 14:57:46 -0500

    Created JSON Location data object
    Using Python, created location_data.json so that we can have the
    preliminary information about the location of the house.

--- HVAC Model Designer/Python_Scripts/Generating_Location_Database.py
+++ HVAC Model Designer/Python_Scripts/Generating_Location_Database.py
@@ -1,5 +1,19 @@
 import csv
-exampleFile = open('example.csv')
-exampleReader = csv.reader(exampleFile)
-for row in exampleReader:
-        print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
+import json
+
+with open('cities.csv', 'r') as csvfile:
+    exampleReader = csv.reader(csvfile)
+    data = []
+    for row in exampleReader:
+        row_data = []
+        for col in row:
+            row_data.append(col)
+        print("Row # {0}".format(str(row)))
+        data.append(row_data)
+    with open('location_data.json', 'w') as f:
+        json.dump(data, f)
+
+with open('location_data.json', 'r') as f:
+    data = json.load(f)
+    for row in data:
+        print("Row # {0}".format(str(row)))
