e2be99e126ce189108b9cd99c243a40d5633249c
Andrew Massey <amassey7@ksu.edu>
2016-10-05 11:18:22 -0500

    Add a bunch of cool things to my LocationPopOver

--- HVAC Model Designer/Javascript/HVACApplication/Interface/LocationDataPopover.js
+++ HVAC Model Designer/Javascript/HVACApplication/Interface/LocationDataPopover.js
@@ -6,6 +6,10 @@ function LocationDataPopover() {
     "use strict";
     //Constructor
     this.locationDataDiv = null;
+    this.saveDataButton = null;
+    this.titleSpan = null;
+    this.backgroundDiv = document.createElement("div");
+    this.backgroundDiv.className = "BackgroundCover";
 
     this.locationDataDiv = document.createElement("div");
     this.locationDataDiv.className = "LocationDataDiv";
@@ -18,11 +22,45 @@ function LocationDataPopover() {
         self.hide();
     };
     this.locationDataDiv.appendChild(this.saveDataButton);
+
+    this.titleSpan = document.createElement("span");
+    this.titleSpan.className = "LocationTitle";
+    this.titleSpan.innerText = "Input Location Data";
+    this.locationDataDiv.appendChild(this.titleSpan);
+
+    this.firstElement = document.createElement("state");
+    this.firstElement.className = "State";
+    this.firstElement.innerText = "Select your State";
+    this.locationDataDiv.appendChild(this.firstElement);
+
+    this.firstButton = document.createElement("select");
+    this.firstButton.setAttribute("id", "State");
+    this.firstButton.className = "StateDropDown";
+    this.firstButton.innerText = "Select a State from Dropdown";
+    this.option1 = document.createElement("option");
+    this.option1.setAttribute("value", "Alabama");
+    this.text1 = document.createTextNode("Alabama");
+    this.option1.appendChild(this.text1);
+    this.firstButton.appendChild(this.option1);
+    this.locationDataDiv.appendChild(this.firstButton);
+
+    this.secondElement = document.createElement("city");
+    this.secondElement.className = "City";
+    this.secondElement.innerText = "Select the City closest to yours";
+    this.locationDataDiv.appendChild(this.secondElement);
+
+    this.secondButton = document.createElement("select");
+    this.secondButton.setAttribute("id", "City");
+    this.secondButton.className = "CityDropDown";
+    this.secondButton.innerText = "Select a City from Dropdown";
+    this.locationDataDiv.appendChild(this.secondButton);
 }
 
 LocationDataPopover.prototype.show = function() {
+    document.body.appendChild(this.backgroundDiv);
     document.body.appendChild(this.locationDataDiv);
 };
 LocationDataPopover.prototype.hide = function() {
+    this.backgroundDiv.remove();
     this.locationDataDiv.remove();
 };
