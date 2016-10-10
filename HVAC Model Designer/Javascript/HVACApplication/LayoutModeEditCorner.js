6651bc5be491cd4eed5a1d158fce19944bd54090
MatthewFrench <matthewf@ksu.edu>
2016-10-10 11:18:26 -0500

    Added wall connection detection
    Next feature is wall rescaling

--- HVAC Model Designer/Javascript/HVACApplication/LayoutModeEditCorner.js
+++ HVAC Model Designer/Javascript/HVACApplication/LayoutModeEditCorner.js
@@ -89,6 +89,67 @@ HVACApplication.prototype.mousePressedEditCornerModeLayout = function () {
             closestCornerPoint = new CornerPoint(wall, WallPointEnum.POINT2);
         }
     }
+
+    //Store all wall connections
+    this.wallConnections = [];
+    for (var i = 0; i < this.wallList.length; i++) {
+        var firstWall = this.wallList[i];
+        for (var j = i+1; j < this.wallList.length; j++) {
+            var secondWall = this.wallList[j];
+            if (firstWall == secondWall) continue;
+
+            //If second wall is connected to first wall but not on an end point, add it
+            //If first will is connected to second wall but not on an end point, add it
+
+            //Second wall point 1 to first wall
+            //Second wall point 2 to first wall
+            //First wall point 1 to second wall
+            //First wall point 2 to second wall
+            var secondWallPoint1Connection = null;
+            var secondWallPoint2Connection = null;
+            var firstWallPoint1Connection = null;
+            var firstWallPoint2Connection = null;
+
+            var firstWallPoint1 = new CornerPoint(firstWall, WallPointEnum.POINT1);
+            var firstWallPoint2 = new CornerPoint(firstWall, WallPointEnum.POINT2);
+            var secondWallPoint1 = new CornerPoint(secondWall, WallPointEnum.POINT1);
+            var secondWallPoint2 = new CornerPoint(secondWall, WallPointEnum.POINT2);
+
+            var checkConnection = function(checkWallPoint, targetWallPoint1, targetWallPoint2) {
+                var pointOnLine = nearestPointOnLine(targetWallPoint1.getX(), targetWallPoint1.getY(),
+                    targetWallPoint2.getX(), targetWallPoint2.getY(), checkWallPoint.getX(), checkWallPoint.getY());
+                var distBetweenConnection = Math.hypot(pointOnLine.x - checkWallPoint.getX(), pointOnLine.y - checkWallPoint.getY());
+                if (distBetweenConnection <= 1.0) { //If within 1 pixel, be connected to wall
+                    //Check if an end point
+                    if (Math.hypot(checkWallPoint.getX() - targetWallPoint1.getX(), checkWallPoint.getY() - targetWallPoint1.getY()) <= 1.0 ||
+                        Math.hypot(checkWallPoint.getX() - targetWallPoint2.getX(), checkWallPoint.getY() - targetWallPoint2.getY()) <= 1.0) {
+                        //End point do nothing
+                    } else {
+                        //Not an end point, add
+                        var connection = new WallConnection(checkWallPoint, targetWallPoint1.wall);
+                        return connection;
+                    }
+                }
+                return null;
+            };
+
+            //Second wall point 1 to first wall
+            secondWallPoint1Connection = checkConnection(secondWallPoint1, firstWallPoint1, firstWallPoint2);
+            secondWallPoint2Connection = checkConnection(secondWallPoint2, firstWallPoint1, firstWallPoint2);
+            firstWallPoint1Connection = checkConnection(firstWallPoint1, secondWallPoint1, secondWallPoint2);
+            firstWallPoint2Connection = checkConnection(firstWallPoint2, secondWallPoint1, secondWallPoint2);
+            if (secondWallPoint1Connection != null) {
+                this.wallConnections.push(secondWallPoint1Connection);
+            } else if (secondWallPoint2Connection != null) {
+                this.wallConnections.push(secondWallPoint2Connection);
+            } else if (firstWallPoint1Connection != null) {
+                this.wallConnections.push(firstWallPoint1Connection);
+            } else if (firstWallPoint2Connection != null) {
+                this.wallConnections.push(firstWallPoint2Connection);
+            }
+        }
+    }
+
     //Search for points near pixel of closest point
     if (closestCornerPoint != null) {
         for (var i = 0; i < this.wallList.length; i++) {
@@ -134,7 +195,8 @@ HVACApplication.prototype.mouseMovedEditCornerModeLayout = function () {
     //Fix connections without moving that point
 
     //Wall connections list
-    var wallConnectionsList = [];
+    //var wallConnectionsList = [];
+
     
 
     for (var i = 0; i < this.currentEditCornerSelectedCornerPoints.length; i++) {
@@ -142,6 +204,8 @@ HVACApplication.prototype.mouseMovedEditCornerModeLayout = function () {
         cornerPoint.setX(cornerPoint.getX() - movedX);
         cornerPoint.setY(cornerPoint.getY() - movedY);
     }
+
+
 };
 
 //Action taken for when the mouse is released.
@@ -173,6 +237,11 @@ HVACApplication.prototype.drawEditCornerModeLayout = function () {
                var corner = this.currentEditCornerSelectedCornerPoints[j];
                if (corner.wall == wall) highlight = true;
            }
+        }
+        for (var wallConnection in this.wallConnections) {
+            if (this.wallConnections[wallConnection].cornerPoint.wall == wall) {
+                highlight = true;
+            }
         }
 
         wall.draw(ctx, highlight);
