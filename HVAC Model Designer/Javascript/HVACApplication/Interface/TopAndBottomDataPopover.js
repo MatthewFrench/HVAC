f1a14af1c860df45ee28b89405c138d79b086366
Andrew Massey <amassey7@ksu.edu>
2016-10-12 11:09:53 -0500

    Added Attic Elements/Buttons

--- HVAC Model Designer/Javascript/HVACApplication/Interface/TopAndBottomDataPopover.js
+++ HVAC Model Designer/Javascript/HVACApplication/Interface/TopAndBottomDataPopover.js
@@ -15,7 +15,7 @@ function TopAndBottomDataPopover() {
 
     this.saveDataButton = document.createElement('button');
     this.saveDataButton.className = "SaveDataButton";
-    this.saveDataButton.innerText = "Save Location Data";
+    this.saveDataButton.innerText = "Save Attic and Basement Data";
     var self = this;
     this.saveDataButton.onclick = function () {
         self.hide();
@@ -23,8 +23,58 @@ function TopAndBottomDataPopover() {
     this.TopAndBottomDataDiv.appendChild(this.saveDataButton);
 
     this.titleSpan = document.createElement("span");
-    this.titleSpan.className = "TopAndBottomTitle";
-    this.titleSpan.innerText = "Input Attic and Basement Data";
+    this.titleSpan.className = "AtticTitle";
+    this.titleSpan.innerText = "Input Attic Data";
+    this.TopAndBottomDataDiv.appendChild(this.titleSpan);
+
+    this.StudSpacingElement = document.createElement("StudSpacing");
+    this.StudSpacingElement.className = "StudSpacingElement";
+    this.StudSpacingElement.innerText = "Spacing of your Studs";
+    this.TopAndBottomDataDiv.appendChild(this.StudSpacingElement);
+    this.StudSpacingButton = document.createElement("select");
+    this.StudSpacingButton.setAttribute("id", "StudSpacing");
+    this.StudSpacingButton.className = "StudSpacingButton";
+    this.TopAndBottomDataDiv.appendChild(this.StudSpacingButton);
+
+    this.StudHeightElement = document.createElement("StudHeight");
+    this.StudHeightElement.className = "StudHeightElement";
+    this.StudHeightElement.innerText = "Height of your Studs";
+    this.TopAndBottomDataDiv.appendChild(this.StudHeightElement);
+    this.StudHeightButton = document.createElement("select");
+    this.StudHeightButton.setAttribute("id", "StudHeight");
+    this.StudHeightButton.className = "StudHeightButton";
+    this.TopAndBottomDataDiv.appendChild(this.StudHeightButton);
+
+    this.StudWidthElement = document.createElement("StudWidth");
+    this.StudWidthElement.className = "StudWidthElement";
+    this.StudWidthElement.innerText = "Width of your Studs";
+    this.TopAndBottomDataDiv.appendChild(this.StudWidthElement);
+    this.StudWidthButton = document.createElement("select");
+    this.StudWidthButton.setAttribute("id", "StudWidth");
+    this.StudWidthButton.className = "StudWidthButton";
+    this.TopAndBottomDataDiv.appendChild(this.StudWidthButton);
+
+    this.AtticInsulationElement = document.createElement("AtticInsulation");
+    this.AtticInsulationElement.className = "AtticInsulationElement";
+    this.AtticInsulationElement.innerText = "Type of Insulation";
+    this.TopAndBottomDataDiv.appendChild(this.AtticInsulationElement);
+    this.AtticInsulationButton = document.createElement("select");
+    this.AtticInsulationButton.setAttribute("id", "AtticInsulation");
+    this.AtticInsulationButton.className = "AtticInsulationButton";
+    this.TopAndBottomDataDiv.appendChild(this.AtticInsulationButton);
+
+    this.AtticInsulationDepthElement = document.createElement("AtticDepthInsulation");
+    this.AtticInsulationDepthElement.className = "AtticInsulationDepthElement";
+    this.AtticInsulationDepthElement.innerText = "Depth of Attic Insulation";
+    this.TopAndBottomDataDiv.appendChild(this.AtticInsulationDepthElement);
+    this.AtticInsulationDepthButton = document.createElement("select");
+    this.AtticInsulationDepthButton.setAttribute("id", "AtticDepthInsulation");
+    this.AtticInsulationDepthButton.className = "AtticInsulationDepthButton";
+    this.TopAndBottomDataDiv.appendChild(this.AtticInsulationDepthButton);
+
+    this.titleSpan = document.createElement("span");
+    this.titleSpan.className = "BasementTitle";
+    this.titleSpan.innerText = "Input Basement Data";
     this.TopAndBottomDataDiv.appendChild(this.titleSpan);
 }
 
