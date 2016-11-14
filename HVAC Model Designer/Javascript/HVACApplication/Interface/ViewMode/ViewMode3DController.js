5a89249819e89174552d0cda3256089e47caaaaf
MatthewFrench <matthewf@ksu.edu>
2016-11-14 11:13:04 -0600

    First working attempt at 3D

--- HVAC Model Designer/Javascript/HVACApplication/Interface/ViewMode/ViewMode3DController.js
+++ HVAC Model Designer/Javascript/HVACApplication/Interface/ViewMode/ViewMode3DController.js
@@ -8,25 +8,56 @@ HVACApplication.prototype.setLayoutViewModeTo3D = function() {
 
         this.layoutViewMode3DCamera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
         this.layoutViewMode3DCamera.position.z = 400;
+this.layoutViewMode3DCamera.lookAt(0, 0, 0);
         this.layoutViewMode3DScene = new THREE.Scene();
 
         //var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
 
 
     for (var i = 0; i < this.getCurrentWallList().length; i++) {
-        var wall = this.getCurrentWallList()[i];
 
-        var lengthOfWall = wall.getLine().getLength();
-        var wallCenter = wall.getLine().getCenterPoint();
-        var wallRotation = wall.getLine().getRotation();
-        var geometry = new THREE.BoxBufferGeometry( 5, 20, lengthOfWall * 5 );
-        var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+        (function(i){
+            AnimationTimer.StartTimerDelayed(this, i * 1.0, 0.0, function(){}, function(){
 
-        var newMesh = new THREE.Mesh( geometry, material );
-        //this.layoutViewMode3DMesh.position = new THREE.Vector3(0, wallCenter.x * 50, wallCenter.y * 50);
-        //this.layoutViewMode3DMesh.rotation.set(new THREE.Vector3( Math.PI / 2, 0, 0));
+                var wall = this.getCurrentWallList()[i];
 
-        this.layoutViewMode3DScene.add( newMesh );
+                var lengthOfWall = wall.getLine().getLength();
+                var wallCenter = wall.getLine().getCenterPoint();
+                var wallRotation = wall.getLine().getRotation();
+
+                var wallWidth = 5;
+                var wallHeight = 50;
+                var wallLength = lengthOfWall / 10.0;
+
+                var geometry = new THREE.BoxBufferGeometry( wallWidth, wallLength, wallHeight );
+                var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true, opacity: 0.5 } );
+
+                var newMesh = new THREE.Mesh( geometry, material );
+                //newMesh.rotation.set(new THREE.Vector3( 0, 0, 0.1));
+
+                this.layoutViewMode3DScene.add( newMesh );
+
+
+                var locationX = wallCenter.x / 10.0;
+                var locationY = wallCenter.y / 10.0;
+                var rotation = wall.getLine().getRotation();
+                console.log("Wall rotation: " + rotation);
+                AnimationTimer.StartTimer(this, 1.5, function(speed, percent){
+                    var x = locationX * percent;
+                    var y = locationY * percent;
+
+                    newMesh.position.set(x, y, 0);
+                    newMesh.rotation.set(0, 0, rotation * percent);
+                }, function(){
+                    newMesh.position.set(locationX, locationY, 0);
+                    newMesh.rotation.set(0, 0, rotation);
+
+                });
+
+            });
+
+
+        }).call(this, i);
     }
 
 
