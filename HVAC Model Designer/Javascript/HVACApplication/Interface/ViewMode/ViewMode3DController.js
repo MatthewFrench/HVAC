6829bcb16eddf28964d71ab0eccb5c3658b486e9
MatthewFrench <matthewf@ksu.edu>
2016-11-16 10:32:56 -0600

    Fixed some of the perspectives

--- HVAC Model Designer/Javascript/HVACApplication/Interface/ViewMode/ViewMode3DController.js
+++ HVAC Model Designer/Javascript/HVACApplication/Interface/ViewMode/ViewMode3DController.js
@@ -2,22 +2,23 @@
  * Created by personal on 11/11/16.
  */
 
-HVACApplication.prototype.setLayoutViewModeTo3D = function() {
+HVACApplication.prototype.setLayoutViewModeTo3D = function () {
     //this.layoutViewMode3DCamera, this.layoutViewMode3DScene, this.layoutViewMode3DRenderer;
     //this.layoutViewMode3DMesh;
 
-        this.layoutViewMode3DCamera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-        this.layoutViewMode3DCamera.position.z = 400;
-this.layoutViewMode3DCamera.lookAt(0, 0, 0);
-        this.layoutViewMode3DScene = new THREE.Scene();
+    this.layoutViewMode3DCamera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    this.layoutViewMode3DCamera.position.z = 400;
+    this.layoutViewMode3DCamera.lookAt(0, 0, 0);
+    this.layoutViewMode3DScene = new THREE.Scene();
 
-        //var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+    //var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
 
 
     for (var i = 0; i < this.getCurrentWallList().length; i++) {
 
-        (function(i){
-            AnimationTimer.StartTimerDelayed(this, i * 1.0, 0.0, function(){}, function(){
+        (function (i) {
+            AnimationTimer.StartTimerDelayed(this, i * 1.0, 0.0, function () {
+            }, function () {
 
                 var wall = this.getCurrentWallList()[i];
 
@@ -29,26 +30,28 @@ this.layoutViewMode3DCamera.lookAt(0, 0, 0);
                 var wallHeight = 50;
                 var wallLength = lengthOfWall / 10.0;
 
-                var geometry = new THREE.BoxBufferGeometry( wallWidth, wallLength, wallHeight );
-                var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true, opacity: 0.5 } );
+                var geometry = new THREE.BoxBufferGeometry(wallWidth, wallLength, wallHeight);
+                var material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true, opacity: 0.5});
 
-                var newMesh = new THREE.Mesh( geometry, material );
+                var newMesh = new THREE.Mesh(geometry, material);
                 //newMesh.rotation.set(new THREE.Vector3( 0, 0, 0.1));
 
-                this.layoutViewMode3DScene.add( newMesh );
+                this.layoutViewMode3DScene.add(newMesh);
 
 
-                var locationX = wallCenter.x / 10.0;
-                var locationY = wallCenter.y / 10.0;
+                var canvasWidth = this.layoutCanvas.width;
+                var canvasHeight = this.layoutCanvas.height;
+
+                var locationX = (-canvasWidth/2+wallCenter.x) / 10.0;
+                var locationY = -(-canvasHeight/2+wallCenter.y) / 10.0;
                 var rotation = wall.getLine().getRotation();
-                console.log("Wall rotation: " + rotation);
-                AnimationTimer.StartTimer(this, 1.5, function(speed, percent){
+                AnimationTimer.StartTimer(this, 1.5, function (speed, percent) {
                     var x = locationX * percent;
                     var y = locationY * percent;
 
                     newMesh.position.set(x, y, 0);
                     newMesh.rotation.set(0, 0, rotation * percent);
-                }, function(){
+                }, function () {
                     newMesh.position.set(locationX, locationY, 0);
                     newMesh.rotation.set(0, 0, rotation);
 
@@ -61,16 +64,15 @@ this.layoutViewMode3DCamera.lookAt(0, 0, 0);
     }
 
 
+    this.layoutViewMode3DRenderer = new THREE.WebGLRenderer();
+    this.layoutViewMode3DRenderer.setPixelRatio(window.devicePixelRatio);
+    this.layoutViewMode3DRenderer.setSize(window.innerWidth, window.innerHeight);
 
-        this.layoutViewMode3DRenderer = new THREE.WebGLRenderer();
-        this.layoutViewMode3DRenderer.setPixelRatio( window.devicePixelRatio );
-        this.layoutViewMode3DRenderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild(this.layoutViewMode3DRenderer.domElement);
 
-        document.body.appendChild( this.layoutViewMode3DRenderer.domElement );
+    window.addEventListener('resize', CreateFunction(this, this.resizeViewModeLayout3D), false);
 
-        window.addEventListener( 'resize', CreateFunction(this, this.resizeViewModeLayout3D), false );
-
-        this.resizeViewModeLayout3D();
+    this.resizeViewModeLayout3D();
 
     this.currentViewModeLayout = ViewModeType.Mode3D;
 };
@@ -80,20 +82,20 @@ HVACApplication.prototype.drawViewModeLayout3D = function () {
     //this.layoutViewMode3DMesh.rotation.x += 0.005;
     //this.layoutViewMode3DMesh.rotation.y += 0.01;
     /*
-    var timer = Date.now() * 0.0001;
+     var timer = Date.now() * 0.0001;
 
-    this.layoutViewMode3DCamera.position.x = Math.cos( timer ) * 200;
-    this.layoutViewMode3DCamera.position.z = Math.sin( timer ) * 200;
-    this.layoutViewMode3DCamera.lookAt( this.layoutViewMode3DScene.position );
-    */
+     this.layoutViewMode3DCamera.position.x = Math.cos( timer ) * 200;
+     this.layoutViewMode3DCamera.position.z = Math.sin( timer ) * 200;
+     this.layoutViewMode3DCamera.lookAt( this.layoutViewMode3DScene.position );
+     */
     //this.layoutViewMode3DScene.rotation.x += 0.005;
     //this.layoutViewMode3DScene.rotation.y += 0.01;
 
-    this.layoutViewMode3DRenderer.render( this.layoutViewMode3DScene, this.layoutViewMode3DCamera );
+    this.layoutViewMode3DRenderer.render(this.layoutViewMode3DScene, this.layoutViewMode3DCamera);
 };
 
-HVACApplication.prototype.resizeViewModeLayout3D = function() {
+HVACApplication.prototype.resizeViewModeLayout3D = function () {
     this.layoutViewMode3DCamera.aspect = window.innerWidth / window.innerHeight;
     this.layoutViewMode3DCamera.updateProjectionMatrix();
-    this.layoutViewMode3DRenderer.setSize( window.innerWidth, window.innerHeight );
+    this.layoutViewMode3DRenderer.setSize(window.innerWidth, window.innerHeight);
 };
