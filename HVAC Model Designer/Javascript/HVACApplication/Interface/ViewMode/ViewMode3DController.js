2db1b802dc238b2d5d0cf661805b2703f245853a
MatthewFrench <matthewf@ksu.edu>
2016-11-30 10:43:15 -0600

    Fix for 3D overlapping

--- HVAC Model Designer/Javascript/HVACApplication/Interface/ViewMode/ViewMode3DController.js
+++ HVAC Model Designer/Javascript/HVACApplication/Interface/ViewMode/ViewMode3DController.js
@@ -13,17 +13,70 @@ function ViewMode3DController(hvacApplication) {
 
     this.defaultZ = 100;
     this.viewZ = this.defaultZ;
+    this.plane2D = null;
+    this.texture2D = null;
+    this.textureCanvas = null;
+
+    this.setupTextureCanvas();
 }
+
+ViewMode3DController.prototype.setupTextureCanvas = function() {
+    this.textureCanvas = document.createElement('canvas');
+
+    this.texture2D = new THREE.Texture(this.textureCanvas);
+    this.texture2D.minFilter = THREE.LinearFilter;
+    this.texture2D.needsUpdate = true;
+};
+
+ViewMode3DController.prototype.drawTextureCanvas = function() {
+    //Set width and height by the size of the whole floor
+    var canvasWidth = 1000;
+    var canvasHeight = 1000;
+    this.textureCanvas.width = canvasWidth;
+    this.textureCanvas.height = canvasHeight;
+
+    var ctx = this.textureCanvas.getContext("2d");
+
+     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+     ctx.save();
+
+     ctx.translate(canvasWidth/2, canvasHeight/2);
+
+     ctx.rotate(this.viewAngle); //convertToRadians(this.viewAngle)
+
+     ctx.scale(this.viewScale, this.viewScale);
+
+     ctx.translate(-canvasWidth/2, -canvasHeight/2);
+
+     //Draw walls
+    for (var i = 0; i < this.hvacApplication.getCurrentWallList().length; i++) {
+        var wall = this.hvacApplication.getCurrentWallList()[i];
+        wall.draw(ctx, false);
+    }
+
+     ctx.restore();
+
+    this.texture2D.needsUpdate = true;
+};
 
 /*This function allows us to show the 3D View Mode*/
 ViewMode3DController.prototype.show = function() {
     this.create3DEverything();
+
+    this.hvacApplication.layoutCanvas.style.display = "none";
+
+    this.drawTextureCanvas();
 };
 
 /*This function allows us to hide the 3D View Mode*/
 ViewMode3DController.prototype.hide = function() {
     if (this.layoutViewMode3DRenderer != null) this.layoutViewMode3DRenderer.domElement.remove();
     this.layoutViewMode3DRenderer = null;
+
+    this.hvacApplication.layoutCanvas.style.display = "";
+
+    this.plane2D = null;
 };
 
 /*This function allows us to handle scrolling in 3D View Mode*/
@@ -50,12 +103,22 @@ ViewMode3DController.prototype.create3DEverything = function () {
         this.layoutViewMode3DRenderer.domElement.remove();
     }
 
-    this.layoutViewMode3DCamera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    var width = window.innerWidth;
+    var height = window.innerHeight - 85;
+    this.layoutViewMode3DCamera = new THREE.PerspectiveCamera(70, width / height, 1, 1000);
     this.layoutViewMode3DCamera.position.z = this.viewZ;
     this.layoutViewMode3DCamera.lookAt(0, 0, 0);
     this.layoutViewMode3DScene = new THREE.Scene();
 
     //var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+
+    var material = new THREE.MeshLambertMaterial({ map : this.texture2D });
+    var plane = new THREE.Mesh(new THREE.PlaneGeometry(this.textureCanvas.width, this.textureCanvas.height), material);
+    plane.doubleSided = true;
+    plane.position.x = 0;
+    plane.rotation.z = 0;
+    this.layoutViewMode3DScene.add(plane);
+    this.plane2D = plane;
 
 
     for (var i = 0; i < this.hvacApplication.getCurrentWallList().length; i++) {
@@ -110,10 +173,10 @@ ViewMode3DController.prototype.create3DEverything = function () {
 
     this.layoutViewMode3DRenderer = new THREE.WebGLRenderer({ alpha: true });
     this.layoutViewMode3DRenderer.setPixelRatio(window.devicePixelRatio);
-    this.layoutViewMode3DRenderer.setSize(window.innerWidth, window.innerHeight);
+    this.layoutViewMode3DRenderer.setSize(width, height);
 
     this.layoutViewMode3DRenderer.domElement.style.position = "absolute";
-    this.layoutViewMode3DRenderer.domElement.style.top = "0";
+    this.layoutViewMode3DRenderer.domElement.style.top = "85px";
     this.layoutViewMode3DRenderer.domElement.style.left = "0";
 
     document.body.appendChild(this.layoutViewMode3DRenderer.domElement);
@@ -157,13 +220,20 @@ ViewMode3DController.prototype.drawLayout = function () {
 
     this.layoutViewMode3DCamera.position.setZ(this.viewZ);
 
+    this.drawTextureCanvas();
+    this.texture2D.needsUpdate = true;
+
     this.layoutViewMode3DRenderer.render(this.layoutViewMode3DScene, this.layoutViewMode3DCamera);
 };
 
 /*This function allows us to resize the 3D View Mode*/
 ViewMode3DController.prototype.resizeView = function () {
     if (this.layoutViewMode3DRenderer == null) return;
-    this.layoutViewMode3DCamera.aspect = window.innerWidth / window.innerHeight;
+
+    var width = window.innerWidth;
+    var height = window.innerHeight - 85;
+
+    this.layoutViewMode3DCamera.aspect = width / height;
     this.layoutViewMode3DCamera.updateProjectionMatrix();
-    this.layoutViewMode3DRenderer.setSize(window.innerWidth, window.innerHeight);
+    this.layoutViewMode3DRenderer.setSize(width, height);
 };
